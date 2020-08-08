09445bb07eb3f5a53cd4b5fe34982d4d9240f213
Mercy <jensen@larkin.space>
2020-08-08 19:48:00 +0100

    Oops

--- src/handlers/steamHandler/user/friendMessage.js
+++ src/handlers/steamHandler/user/friendMessage.js
@@ -1,9 +1,10 @@
 module.exports.init = (steamUser, csgoClient, bot, config) => {
   steamUser.chat.on('friendMessage', (message) => {
 
-    if (message.message.length == 0) return;
+    if (message.message.trim().length == 0) return;
     bot.db.getUserFromSteamID(message.steamid_friend, (err, user) => {
       if (err) return bot.log.error(err);
+      if(!user) return;
       if (user.authcode == message.message.trim()) {
         steamUser.chat.sendFriendMessage(message.steamid_friend, 'Successfully linked your account! You can now use cs.update in the Discord server to update your rank!');
         csgoClient.requestPlayersProfile(message.steamid_friend, (result) => {
