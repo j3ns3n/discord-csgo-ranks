6ca949cfada2451ae6ede85456257e5181a9ddbc
Mercy <jensen@larkin.space>
2020-08-08 04:35:26 +0100

    Fixed ranks not being properly registered

--- src/handlers/playerDataHandler/index.js
+++ src/handlers/playerDataHandler/index.js
@@ -6,16 +6,16 @@ const handler = (bot, config, csgoClient, profile) => {
 
 
   if (profile.ranking == null) return;
-  if (profile.rankings.filter(r => r.rank_type_id == 6).rank_id == null) return;
+  if (profile.ranking.rank_id == null) return;
 
   bot.db.getUserFromCSGOID(account_id, (err, user) => {
     if (err) {
       return bot.log.error(err);
     }
 
-    if (user.rank == profile.rankings.filter(r => r.rank_type_id == 6).rank_id) return;
+    if (user.rank == profile.ranking.rank_id) return;
 
-    let rank_id = profile.ranking.filter(r => r.rank_type_id == 6).rank_id;
+    let rank_id = profile.ranking.rank_id;
 
 
     bot.db.updateUserRank(bot, account_id, rank_id);
--- src/handlers/steamHandler/csgo/playersProfile.js
+++ src/handlers/steamHandler/csgo/playersProfile.js
@@ -1,5 +1,6 @@
 module.exports.init = (steamUser, csgoClient, bot, config) => {
   csgoClient.on('playersProfile', (profile) => {
+    // console.log(profile)
     bot.playerDataHandler(bot, config, csgoClient, profile);
   });
 };
