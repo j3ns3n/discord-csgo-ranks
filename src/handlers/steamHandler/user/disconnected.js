b76954c1e5fd64e2784163bcfb7067b53079de1c
Mercy <jensen@larkin.space>
2020-08-23 19:52:06 +0100

    Added handling for "Invalid" Steam login error code.

--- src/handlers/steamHandler/user/disconnected.js
+++ src/handlers/steamHandler/user/disconnected.js
@@ -1,6 +1,12 @@
 module.exports.init = (steamUser, csgoClient, bot, config) => {
   steamUser.on('disconnected', (err) => {
     bot.log.warn('Steam Client logged off with error code: [' + err + '] ' + steamUser.EResult[err]);
+    if (err == 0) { 
+      bot.users.get(config.ownerid).getDMChannel((ownerDM) => {
+        ownerDM.createMessage(':exclamation: | Invalid login code error, bot will restart.');
+      });
+      process.exit(1);
+    }
     steamUser.logOff();
     steamUser.logOn();
   });
