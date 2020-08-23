d9de42b4d4ca441dfa4db2a46a8eb520a013695d
Mercy <jensen@larkin.space>
2020-08-23 19:54:11 +0100

    Fix for incorrect config location

--- src/handlers/steamHandler/user/disconnected.js
+++ src/handlers/steamHandler/user/disconnected.js
@@ -2,7 +2,7 @@ module.exports.init = (steamUser, csgoClient, bot, config) => {
   steamUser.on('disconnected', (err) => {
     bot.log.warn('Steam Client logged off with error code: [' + err + '] ' + steamUser.EResult[err]);
     if (err == 0) { 
-      bot.users.get(config.ownerid).getDMChannel((ownerDM) => {
+      bot.users.get(config.discord.ownerid).getDMChannel((ownerDM) => {
         ownerDM.createMessage(':exclamation: | Invalid login code error, bot will restart.');
       });
       process.exit(1);
